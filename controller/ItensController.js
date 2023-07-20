4c7062141081af2c2c561bcc0fcd1fe593328a79
allerman <allerman3301@gmail.com>
2023-07-20 01:00:20 -0300

    feat: add creatItem e ajustando o getAll

--- controller/ItensController.js
+++ controller/ItensController.js
@@ -1,7 +1,33 @@
-const getAll = (req,res) => {
-    return res.render("index");
+const Quarto = require('../model/Quarto');
+
+const getAll = async (req,res) => {
+    try{
+        const ListadeItens = await Quarto.find(); 
+        return res.render("index", {ListadeItens});
+    }
+    catch(err){
+        res.status(500).send({error: err.message})
+    }
+
+};
+
+const creatItem = async (req,res)=>{
+    const item=req.body;
+    
+    if(!item.item){
+        return res.redirect("/");
+    }
+    try{
+        await Quarto.create(item);
+        return res.redirect("/");
+    }
+    catch(err){
+        res.status(500).send({error: err.message});
+    }
+    
 };
 
 module.exports = {
     getAll,
+    creatItem,
 };
