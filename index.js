4aa43e606a2ac043327633c4abe97850877b9e83
allerman <allerman3301@gmail.com>
2023-07-19 21:09:06 -0300

    feat: adicionando a funçao de verificaçao de conexao ao DB

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 const path = require("path")
 const routes = require("./routes/routes")
+const connectToDb = require("./database/db")
 
+connectToDb();
 app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname,"public")));
 app.use(routes)
