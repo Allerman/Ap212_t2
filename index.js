1e6f593224ca6cf2b1290bdad079717e30140343
allerman <allerman3301@gmail.com>
2023-07-20 00:51:37 -0300

    adicionando urlencoded

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require("path")
-const routes = require("./routes/routes")
-const connectToDb = require("./database/db")
+const path = require("path");
+const routes = require("./routes/routes");
+const connectToDb = require("./database/db");
 
 connectToDb();
-app.set("view engine","ejs")
+app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
-app.use(routes)
+app.use(express.urlencoded());
+app.use(routes);
 
 app.listen(port, () =>{console.log(`rodando em http://localhost:${port}`)});
 
