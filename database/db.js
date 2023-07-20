45df5e5ea8af9ba06c4316c967cb24672d742c54
allerman <allerman3301@gmail.com>
2023-07-19 21:07:48 -0300

    feat: adicionando controole de erros e aviso de conexao bem sucedida com o mongoDB

--- database/db.js
+++ database/db.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const linkConectDB = process.env.DB_LINK;
+
 const mongoose = require('mongoose');
 
 const connectToDb = () => {
     mongoose.connect(
-        linkConectDB
-    )
+        linkConectDB,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    ).then(()=> console.log("MongoDB conectado, baby!")).catch((err)=> console.log(err));
 }
 
 module.exports = connectToDb;
